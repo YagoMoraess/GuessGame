9ba1a12dcb5d1c265ea708aa040ade30f1b85ed6
Yago Moraes <yago.mbarreto@hotmail.com>
2024-09-25 20:50:54 -0300

    Adicionando o reset, attempts e melhorando algumas coisas.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,12 @@ use std::cmp::Ordering;
 use eframe::egui::{self};
 use rand::Rng;
 
+struct GameStatus {
+    result: String,
+    attempts: i32,
+    secret_number: u32,
+}
+
 fn main() -> eframe::Result {
     env_logger::init();
     let options = eframe::NativeOptions {
@@ -11,44 +17,54 @@ fn main() -> eframe::Result {
     };
 
     let min_number = 1;
-    let max_number = 500;
+    let max_number = 10;
+
     let mut guess = String::new();
-    let secret_number = rand::thread_rng().gen_range(min_number..max_number);
-    let mut result = "";
+    let mut game_status = GameStatus { result: String::new(), attempts: 0, secret_number: rand::thread_rng().gen_range(min_number..max_number) };
 
     eframe::run_simple_native("Guess Game", options, move |ctx, _frame| {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.horizontal(|ui| {
-                let mut guess_label = ui.label(format!("Digite um numero entre {min_number} e {max_number}: "));
+                let mut guess_label = ui.label(format!("Choose a number between {min_number} and {max_number}: "));
                 let response = ui.text_edit_singleline(&mut guess).labelled_by(guess_label.id);
                 if response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
-                    result = process_guess(&guess, &secret_number);
+                    process_guess(&guess, &mut game_status);
                 }
-
             });
             if ui.button("Try out!").clicked() {
-                result = process_guess(&guess, &secret_number);
+                process_guess(&guess, &mut game_status);
             }
-            ui.label(format!("{result}"));
+            if ui.button("Reset").clicked() {
+                reset_game(&mut game_status, min_number, max_number);
+            }
+            ui.label(format!("Attempts: {}", game_status.attempts));
+            ui.label(format!("{}", game_status.result));
         });
     })
 }
 
-fn process_guess(guess: &String, secret_number: &u32) -> &'static str {
-    let guess: u32 = guess.trim().parse().expect("Erro");
-    let mut result = "";
+fn process_guess(guess: &String, game_status: &mut GameStatus) {
+    let guess: u32 = guess.trim().parse().expect("Error");
 
-    match guess.cmp(secret_number) {
+    match guess.cmp(&game_status.secret_number) {
         Ordering::Less =>  {
-            result = "Too small!";
+            game_status.attempts += 1;
+            game_status.result = "Too small!".to_string();
         },
         Ordering::Equal => {
-            result = "You win!";
+            game_status.attempts += 1;
+            game_status.result = "You win!".to_string();
         },
         Ordering::Greater => {
-            result = "To big!";
+            game_status.attempts += 1;
+            game_status.result = "To big!".to_string();
 
         },
     }
-    return result;
+}
+
+fn reset_game(game_status: &mut GameStatus, min_number: u32, max_number: u32) {
+    game_status.attempts = 0;
+    game_status.result = String::new();
+    game_status.secret_number = rand::thread_rng().gen_range(min_number..max_number);
 }
